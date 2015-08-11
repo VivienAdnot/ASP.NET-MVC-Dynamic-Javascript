9131f49616493abef6827ac86e121d921cd95fae
unknown <vivien.adnot@gmail.com>
2015-08-11 17:04:46 +0200

    created custom route to load the script

--- DynamicJavascript/DynamicJavascript/App_Start/RouteConfig.cs
+++ DynamicJavascript/DynamicJavascript/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace DynamicJavascript
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "buildJS",
+                url: "script",
+                defaults: new { controller = "Home", action = "GetScript" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
